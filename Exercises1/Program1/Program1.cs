6b2961f6e1d7d9521b8efbb5128ce3fceb098416
mkiefer3711 <74162397+mkiefer3711@users.noreply.github.com>
2024-07-18 15:00:52 -0400

    Add files via upload

---
+++ Exercises1/Program1/Program1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Program1 
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Excercise 1 - Marathons - Program 1\n");
+
+            // Receiving the race time
+            Console.WriteLine("Please Enter the Time of the Race (Format: hh:mm:ss): ");
+            var timeString = Console.ReadLine();
+            TimeSpan.TryParse(timeString, out TimeSpan time);
+           
+            // Calculating the total time in seconds
+            int totalTimeSecs = (int)time.TotalSeconds;
+
+            // Receiving the distance
+            Console.Write("What is the distance of the race? ");
+            double distance = Convert.ToDouble(Console.ReadLine());
+
+            // Receiving the unit
+            Console.Write("Is the distance in miles or kilometers? (mi / km): ");
+            var unit = Console.ReadLine();
+
+            // Checks the unit and will convert if necessary
+            double distanceInMiles = (unit == "km") ? distance * 0.6214 : distance;
+            double distanceInKm = (unit == "mi") ? distance * 1.60934 : distance;
+
+            // Calculates the pace in seconds for kilometers and miles
+            double paceInSecPerMile = totalTimeSecs / distanceInMiles;
+            double paceInSecPerKm = totalTimeSecs / distanceInKm;
+
+            // Calculates minutes and seconds per kilometer or mile
+            double minPerMile = paceInSecPerMile / 60;
+            double secPerMile = paceInSecPerMile % 60;
+            double minPerKm = paceInSecPerKm / 60;
+            double secPerKm = paceInSecPerKm % 60;
+
+            // Displays information to the console
+            Console.WriteLine("\nTime: " + time);
+            Console.WriteLine("Distance in miles: " + distanceInMiles.ToString("#.#") + "  Distance in km: " + distanceInKm.ToString("#.#"));
+            Console.WriteLine("Pace in minutes per mile: " + (int)minPerMile + ":" + secPerMile.ToString("#.") + "  Pace in minutes per km: " + (int)minPerKm + ":" + secPerKm.ToString("#."));
+            Console.WriteLine("\nDone");
+        }
+    }
+}
---
+++ Exercises1/Program2/Program2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Program2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Excercise 1 - Marathons - Program 2\n");
+
+            // Creating arrays for hours, minutes and seconds
+            var times = new TimeSpan[10];
+            int enteredRaces = 0;
+
+            // While the enteredRaces is less than 10, it will continue to ask for times
+            while (enteredRaces < 10)
+            {
+                Console.WriteLine("Please enter the time for race " + (enteredRaces + 1) + " (Format: hh:mm:ss): ");
+                var input = Console.ReadLine();
+
+                // If -1 is input to the console, the loop will end
+                if (input == "-1")
+                {
+                    break;
+                }
+
+                // Convert and store the input time
+                TimeSpan.TryParse(input, out times[enteredRaces]);
+                enteredRaces++;
+            }
+
+            int fastestIndex = 0;
+            int slowestIndex = 0;
+            int totalSeconds = 0;
+
+            // Iterate through the input race times and find the fastest and slowest time
+            for (int i = 0; i < enteredRaces; i++)
+            {
+                int timeInSeconds = (int)times[i].TotalSeconds;
+
+                if (timeInSeconds < times[fastestIndex].TotalSeconds)
+                {
+                    fastestIndex = i;
+                }
+
+                if (timeInSeconds > times[slowestIndex].TotalSeconds)
+                {
+                    slowestIndex = i;
+                }
+
+                // Calculates total seconds
+                totalSeconds += timeInSeconds;
+
+            }
+
+            // Calculate the average time in seconds
+            int averageTimeInSecs = totalSeconds / enteredRaces;
+            var averagetime = TimeSpan.FromSeconds(averageTimeInSecs);
+
+            Console.WriteLine();
+
+            // Iterate through entered races to print race info and mark the fastest and slowest races
+            for (int i = 0; i < enteredRaces; i++)
+            {
+                string info = "Race " + (i + 1) + ": " + times[i].Hours + ":" + times[i].Minutes + ":" + times[i].Seconds;
+
+                if (i == fastestIndex)
+                {
+                    info += " **FASTEST**";
+                }
+
+                else if (i == slowestIndex)
+                {
+                    info += " **SLOWEST**";
+                }
+
+                // Display the race info
+                Console.WriteLine(info);
+            }
+
+            // Display the average time
+            Console.WriteLine("\nAverage Time: " + averagetime );
+            Console.WriteLine("\nDone");
+        }
+    }
+}
