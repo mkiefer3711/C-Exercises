adf3029b67365b8da461bcd5b43a5e44c4200730
mkiefer3711 <74162397+mkiefer3711@users.noreply.github.com>
2024-07-18 15:02:41 -0400

    Add files via upload

---
+++ Exercises3/Program1/Program1.cs
@@ -0,0 +1,230 @@
+﻿// Author: Maddison Kiefer
+// Program for a university with four departments. Displays the staff, their salary, and what they do.
+
+using System;
+
+namespace Program1
+{
+    // Interface for objects that can be taught
+    public interface ITeachable
+    {
+        // Method for teaching
+        void Teach();
+    }
+    // Interface for objects that can administrate
+    public interface IAdmin
+    {
+        // Method for administration
+        void Administrate();
+    }
+    // Class representing a staff member
+    public class Staff
+    {
+        // Name of the staff member
+        public string Name { get; set; }
+        // Salary of the staff member
+        public double Salary { get; set; }
+
+        // Constructor to initialize Name and Salary
+        public Staff(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+        // Method to display information about the staff member
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine("Salary is: " + Salary);
+        }
+    }
+
+    // Class representing a Professor, inherits from Staff and implements ITeachable
+    public class Professor : Staff, ITeachable
+    {
+        // Class taught by the professor
+        public string Class { get; set; }
+
+        // Constructor to initialize Name, Salary, and Class
+        public Professor(string name, double salary, string className) : base(name, salary)
+        {
+            Class = className;
+        }
+
+        // Override method to display professor's information
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("I am a professor, and I teach classes");
+            Teach();
+        }
+        // Implementation of Teach method from ITeachable interface
+        public void Teach()
+        {
+            Console.WriteLine("I teach " + Class);
+        }
+    }
+
+    // Class representing a Researcher, inherits from Staff and implements ITeachable
+    public class Researcher : Staff, ITeachable
+    {
+        // Enum for different research specialties
+        public enum ResearchSpeciality { NEUROSCIENCE, ROBOTICS, PSYCHOLOGY, CHEMISTRY }
+        // Research speciality of the researcher
+        public ResearchSpeciality Speciality { get; set; }
+
+        // Constructor to initialize Name, Salary, and Speciality
+        public Researcher(string name, double salary, ResearchSpeciality speciality) : base(name, salary)
+        {
+            Speciality = speciality;
+        }
+
+        // Override method to display researcher's information
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("I am a researcher, and I do research");
+            Teach();
+        }
+        // Implementation of Teach method from ITeachable interface
+        public void Teach()
+        {
+            Console.WriteLine("I do research in " + Speciality);
+        }
+    }
+
+    // Class representing a Dean, inherits from Staff and implements ITeachable and IAdmin
+    public class Dean : Staff, ITeachable, IAdmin
+    {
+        // Constructor to initialize Name and Salary
+        public Dean(string name, double salary) : base(name, salary)
+        {
+        }
+
+        // Override method to display dean's information, including teaching and administrative details
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Teach();
+            Administrate();
+        }
+        // Implementation of Teach method from ITeachable interface
+        public void Teach()
+        {
+            Console.WriteLine("I am a dean, and I teach classes");
+        }
+        // Implementation of Administrate method from IAdmin interface
+        public void Administrate()
+        {
+            Console.WriteLine("I am a dean and an administrator");
+        }
+    }
+
+    // Class representing an Administrator, inherits from Staff and implements IAdmin
+    public class Administrator : Staff, IAdmin
+    {
+        // Constructor to initialize Name and Salary
+        public Administrator(string name, double salary) : base(name, salary)
+        {
+        }
+        // Override method to display administrator's information, including administrative details
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Administrate();
+        }
+        // Implementation of Administrate method from IAdmin interface
+        public void Administrate()
+        {
+            Console.WriteLine("I am an administrator");
+        }
+    }
+
+    // Class representing a Department
+    public class Department
+    {
+        // Array to hold staff members in the department
+        public Staff[] Staff { get; set; }
+
+        // Constructor to initialize the Staff array with a length of 10
+        public Department()
+        {
+            Staff = new Staff[10];
+        }
+        // Method to display information about the staff members in the department
+        public void DisplayInfo()
+        {
+            foreach (var staff in Staff)
+            {
+                if (staff != null)
+                {
+                    staff.DisplayInfo();
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+    // Class representing a University
+    public class University
+    {
+        // Enum for the four different departments
+        public enum DepartmentType { MATH, ENGLISH, GEOGRAPHY, COMPUTERSCIENCE }
+
+        // Array to hold departments in the university
+        public Department[] Departments { get; set; }
+
+        // Constructor to initialize the Departments array with a length of 4 and create Department objects
+        public University()
+        {
+            Departments = new Department[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Departments[i] = new Department();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Create a new University object
+            var u = new University();
+
+            Console.WriteLine("Module 3 - Exercise 1 - University\n");
+
+            // Assign staff members to departments and display information
+            u.Departments[0].Staff[0] = new Professor("Daniel", 45000, "Geometry");
+            u.Departments[1].Staff[0] = new Administrator("Brooke", 60000);
+            u.Departments[2].Staff[0] = new Dean("Kristen", 85000);
+            u.Departments[3].Staff[0] = new Researcher("Rachel", 55000, Researcher.ResearchSpeciality.ROBOTICS);
+
+            // Display information for each department
+            Console.WriteLine("Department 1\n");
+            u.Departments[0].DisplayInfo();
+            u.Departments[1].DisplayInfo();
+            u.Departments[2].DisplayInfo();
+            u.Departments[3].DisplayInfo();
+
+            // Create a new University object
+            var u2 = new University();
+
+            // Assign staff members to departments and display information
+            u2.Departments[0].Staff[0] = new Professor("Kelly", 47000, "Trigonometry");
+            u2.Departments[1].Staff[0] = new Administrator("Tyler", 59000);
+            u2.Departments[2].Staff[0] = new Dean("Tom", 86000);
+            u2.Departments[1].Staff[1] = new Researcher("Suzan", 55000, Researcher.ResearchSpeciality.PSYCHOLOGY);
+            u2.Departments[0].Staff[1] = new Dean("Matt", 88000);
+            u2.Departments[3].Staff[0] = new Professor("Taylor", 48000, "CIS102");
+
+            // Display information for each department
+            Console.WriteLine("Department 2\n");
+            u2.Departments[0].DisplayInfo();
+            u2.Departments[1].DisplayInfo();
+            u2.Departments[2].DisplayInfo();
+            u2.Departments[3].DisplayInfo();
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ Exercises3/Program2/Program2.cs
@@ -0,0 +1,246 @@
+﻿// Author: Maddison Kiefer
+// Program for an inventory system for an RPG. Items have their own cost and weight and the program will calculate
+// the TotalCount and TotalWeight to display it to the console
+
+using System;
+
+namespace Program2
+{
+    // Interface to describe the behavior of the container
+    public interface IContainer
+    {
+        // Method to add item to container
+        void Add(Item item);
+        // Property to get the total count of items in the container
+        int TotalCount { get; }
+        // Property to get the total weight of items in the container
+        double TotalWeight { get; }
+    }
+
+    // Abstract class the represents an item
+    public abstract class Item
+    {
+        // Cost of the item
+        public double Cost { get; protected set; }
+        // Weight of the item
+        public double Weight { get; protected set; }
+    }
+
+    // Different types of items derived from the Item class
+    public class Sword : Item
+    {
+        public Sword()
+        {
+            Cost = 50;
+            Weight = 10;
+        }
+    }
+    public class Potion : Item
+    {
+        public Potion()
+        {
+            Cost = 10;
+            Weight = 2;
+        }
+    }
+    public class Bow : Item
+    {
+        public Bow()
+        {
+            Cost = 40;
+            Weight = 5;
+        }
+    }
+    public class Arrow : Item
+    {
+        public Arrow()
+        {
+            Cost = 3;
+            Weight = 1;
+        }
+    }
+    public class Staff : Item
+    {
+        public Staff()
+        {
+            Cost = 45;
+            Weight = 7;
+        }
+    }
+
+    // BagOfHolding that is both an item and a container
+    public class BagOfHolding : Item, IContainer
+    {
+        // List to hold the items in the bag
+        private List<Item> items;
+        
+        public BagOfHolding(int capacity)
+        {
+            //Initialize the list with a specific capacity
+            items = new List<Item>(capacity);
+            // Sets the cost of the bag
+            Cost = 100;
+            // Sets the weight of the bag
+            Weight = 5;
+        }
+        public void Add(Item item)
+        {
+            // Add an item to the bag
+            items.Add(item);
+        }
+        public int TotalCount
+        {
+            get
+            {
+                // Initialize count with the number of items directly in the bag
+                int count = items.Count;
+                foreach (var item in items)
+                {
+                    if (item is IContainer container)
+                    {
+                        // Recursively add the count of items in contained containers
+                        count += container.TotalCount;
+                    }
+                }
+                // Returns the total count
+                return count;
+            }
+        }
+        public double TotalWeight
+        {
+            get
+            {
+                // Initialize weight with the weight of the bag itself
+                double weight = Weight;
+                foreach (var item in items)
+                {
+                    // Add the weight of each item in the bag
+                    weight += item.Weight;
+                    if (item is IContainer container)
+                    {
+                        // Recursively add the weight of items in contained containers
+                        weight += container.TotalWeight;
+                    }
+                }
+                // Return the total weight
+                return weight;
+            }
+        }
+    }
+
+    // Inventory class which is also a container
+    public class Inventory : IContainer
+    {
+        // List to hold the items in the inventory
+        private List<Item> items;
+
+        public Inventory(int capacity)
+        {
+            // Initialize the list with a specified capacity
+            items = new List<Item>(capacity);
+        }
+
+        public void Add(Item item)
+        {
+            // Add an item to the inventory
+            items.Add(item);
+        }
+
+        public int TotalCount
+        {
+            get
+            {
+                // Initialize count with the number of items directly in the inventory
+                int count = items.Count;
+                foreach (var item in items)
+                {
+                    if (item is IContainer container)
+                    {
+                        // Recursively add the count of items in contained containers
+                        count += container.TotalCount;
+                    }
+                }
+                // Return the total count
+                return count;
+            }
+        }
+
+        public double TotalWeight
+        {
+            get
+            {
+                // Initialize weight as zero
+                double weight = 0;
+                foreach (var item in items)
+                {
+                    
+                    if (item is IContainer container)
+                    {
+                        // Recursively add the weight of items in contained containers
+                        weight += container.TotalWeight;
+                    }
+                    else
+                    {
+                        // Add the weight of each item directly in the inventory
+                        weight += item.Weight;
+                    }
+                }
+                // Return the total weight
+                return weight;
+            }
+        }
+    }
+
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            // Testing the classes
+            // Creates the inventory and sets capacity
+            var inventory = new Inventory(3);
+            // Adds items to the inventory
+            inventory.Add(new Sword());
+            inventory.Add(new Potion());
+            // Creates a new bagOfHolding and sets capacity
+            var bagOfHolding = new BagOfHolding(4);
+            // Adds the bag to the inventory
+            inventory.Add(bagOfHolding);
+            // Adds an item to the bag of holding
+            bagOfHolding.Add(new Sword());
+
+            Console.WriteLine("Module 3 - Exercise 2 - Inventory\n");
+            // Displaying the info the the console
+            Console.WriteLine("First Inventory\n");
+            Console.WriteLine("Total number of items in Bag: {0}", bagOfHolding.TotalCount);
+            Console.WriteLine("Total weight of Bag and items inside: {0} pounds", bagOfHolding.TotalWeight);
+            Console.WriteLine();
+            Console.WriteLine("The number of items in the inventory is {0}", inventory.TotalCount);
+            Console.WriteLine("The total weight of the inventory is {0}", inventory.TotalWeight);
+
+            // Creates the inventory and sets capacity
+            var inventory2 = new Inventory(8);
+            // Adds items to the inventory
+            inventory2.Add(new Sword());
+            inventory2.Add(new Staff());
+            inventory2.Add(new Bow());
+            inventory2.Add(new Potion());
+            // Creates a new bagOfHolding and sets capacity
+            var bagOfHolding2 = new BagOfHolding(4);
+            // Adds the bag to the inventory
+            inventory2.Add(bagOfHolding2);
+            // Adds items to the bag of holding
+            bagOfHolding2.Add(new Arrow());
+            bagOfHolding2.Add(new Arrow());
+            bagOfHolding2.Add(new Arrow());
+
+            // Displaying the info the the console
+            Console.WriteLine("\nSecond Inventory\n");
+            Console.WriteLine("Total number of items in Bag: {0}", bagOfHolding2.TotalCount);
+            Console.WriteLine("Total weight of Bag and items inside: {0} pounds", bagOfHolding2.TotalWeight);
+            Console.WriteLine();
+            Console.WriteLine("The number of items in the inventory is {0}", inventory2.TotalCount);
+            Console.WriteLine("The total weight of the inventory is {0}", inventory2.TotalWeight);
+            Console.WriteLine("\nDone");
+        }
+    }
+}
